9278c0e142c65007666d1669a6f5b0f96efc81a5
Kevin <kevin94singh@gmail.com>
2018-07-16 15:23:58 -0700

    Main TableView dataSource+delegate updated.

--- StockApp/Controllers/MainViewController.swift
+++ StockApp/Controllers/MainViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if #available(iOS 11.0, *) {
             navigationController?.navigationBar.prefersLargeTitles = true
             navigationItem.largeTitleDisplayMode = .automatic
             navigationController?.navigationBar.barStyle = .black
+            tableView.dataSource = self
+            tableView.delegate = self
         }
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -24,7 +28,36 @@ class MainViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
+
+extension MainViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StockTableViewCell") as! StockTableViewCell
+        cell.textLabel?.text = "Apple"
+        cell.detailTextLabel?.text = "$ 1 mil"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Favorites"
+        } else {
+            return "Other Companies"
+        }
+    }
+}
 
 
+extension MainViewController: UITableViewDelegate {
+    
 }
 
