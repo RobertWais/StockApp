21d270b5954eebe27b9cc2e40ec7333e78b0c475
RobertWais <wais.robert@uwlax.edu>
2018-07-16 13:10:23 -0700

    fist commit

---
+++ StockApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StockApp
+//
+//  Created by Robert Wais on 7/16/18.
+//  Copyright © 2018 MakeSchoolHackathon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
