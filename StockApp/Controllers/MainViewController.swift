5d21ceabb9fd4bdc16518b7b72b704a7178d8e0e
Kevin <kevin94singh@gmail.com>
2018-07-17 16:43:44 -0700

    For Ethan

--- StockApp/Controllers/MainViewController.swift
+++ StockApp/Controllers/MainViewController.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import Charts
 
-class MainViewController: UIViewController {
-
-    @IBOutlet weak var tableView: UITableView!
+class MainViewController: UIViewController, ChartViewDelegate {
+    
+    @IBOutlet weak var chartView: LineChartView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,51 +19,74 @@ class MainViewController: UIViewController {
             navigationController?.navigationBar.prefersLargeTitles = true
             navigationItem.largeTitleDisplayMode = .automatic
             navigationController?.navigationBar.barStyle = .black
-            tableView.dataSource = self
-            tableView.delegate = self
         }
+        self.title = "Line Chart 1"
+        
+        chartView.delegate = self
+        
+        chartView.chartDescription?.enabled = false
+        chartView.dragEnabled = true
+        chartView.setScaleEnabled(true)
+        chartView.pinchZoomEnabled = true
+        
+        // x-axis limit line
+        let llXAxis = ChartLimitLine(limit: 10, label: "Index 10")
+        llXAxis.lineWidth = 4
+        llXAxis.lineDashLengths = [10, 10, 0]
+        llXAxis.labelPosition = .rightBottom
+        llXAxis.valueFont = .systemFont(ofSize: 10)
+        
+        chartView.xAxis.gridLineDashLengths = [10, 10]
+        chartView.xAxis.gridLineDashPhase = 0
+        
+        let ll1 = ChartLimitLine(limit: 150, label: "Upper Limit")
+        ll1.lineWidth = 4
+        ll1.lineDashLengths = [5, 5]
+        ll1.labelPosition = .rightTop
+        ll1.valueFont = .systemFont(ofSize: 10)
+        
+        let ll2 = ChartLimitLine(limit: -30, label: "Lower Limit")
+        ll2.lineWidth = 4
+        ll2.lineDashLengths = [5,5]
+        ll2.labelPosition = .rightBottom
+        ll2.valueFont = .systemFont(ofSize: 10)
+        
+        let leftAxis = chartView.leftAxis
+        leftAxis.removeAllLimitLines()
+        leftAxis.addLimitLine(ll1)
+        leftAxis.addLimitLine(ll2)
+        leftAxis.axisMaximum = 200
+        leftAxis.axisMinimum = -50
+        leftAxis.gridLineDashLengths = [5, 5]
+        leftAxis.drawLimitLinesBehindDataEnabled = true
+        
+        chartView.rightAxis.enabled = false
+        
+        chartView.legend.form = .line
+        
+        chartView.animate(xAxisDuration: 2.5)
     }
+    
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        NSLog("chartValueSelected");
+    }
+    
+    func chartValueNothingSelected(_ chartView: ChartViewBase) {
+        NSLog("chartValueNothingSelected");
+    }
+    
+    func chartScaled(_ chartView: ChartViewBase, scaleX: CGFloat, scaleY: CGFloat) {
+        
+    }
+    
+    func chartTranslated(_ chartView: ChartViewBase, dX: CGFloat, dY: CGFloat) {
+        
+    }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-}
-
-extension MainViewController: UITableViewDataSource {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
-    }
-    //    TODO: STATIC DATA -> API DATA
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "StockTableViewCell") as! StockTableViewCell
-        cell.accessoryType = .disclosureIndicator
-        switch indexPath.row {
-        case 0:
-        cell.textLabel?.text = "Amazon"
-        cell.detailTextLabel?.text = "$ 1,2 mil"
-        case 1:
-            cell.textLabel?.text = "Apple"
-            cell.detailTextLabel?.text = "$ 1,5 mil"
-        default:
-            print("Out of indexPath")
-        }
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section == 0 {
-            return "Favorites"
-        } else {
-            return "Other Companies"
-        }
-    }
-}
-extension MainViewController: UITableViewDelegate {
-    
 }
 
