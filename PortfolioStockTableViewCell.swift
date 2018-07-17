64728eab987a5061ff80c1bd6dc1c51e3c955727
Kevin <kevin94singh@gmail.com>
2018-07-16 22:20:54 -0700

    Portfolio updated

---
+++ PortfolioStockTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  PortfolioStockTableViewCell.swift
+//  StockApp
+//
+//  Created by Kevin Singh on 7/16/18.
+//  Copyright © 2018 MakeSchoolHackathon. All rights reserved.
+//
+
+import UIKit
+
+class PortfolioStockTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var stockTitle: UILabel!
+    @IBOutlet weak var stockPrice: UILabel!
+    @IBOutlet weak var stockAmount: UILabel!
+    @IBOutlet weak var stockValue: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- StockApp/Controllers/PortfolioViewController.swift
+++ StockApp/Controllers/PortfolioViewController.swift
@@ -12,6 +12,8 @@ class PortfolioViewController: UIViewController {
     
     let addNavigationItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if #available(iOS 11.0, *) {
@@ -22,14 +24,49 @@ class PortfolioViewController: UIViewController {
             navigationItem.rightBarButtonItem?.image = UIImage(named: "addIcon")
             navigationItem.rightBarButtonItem?.tintColor = .white
         }
+        tableView.delegate = self
+        tableView.dataSource = self
 
     }
     
     @objc func addTapped() {
-        
+        print("add button tapped")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+}
+
+extension PortfolioViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Owned Stocks"
+    }
+}
+
+extension PortfolioViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PortfolioStockTableViewCell") as! PortfolioStockTableViewCell
+        if indexPath.row == 0 {
+            cell.stockTitle.text = "AMZN"
+            cell.stockPrice.text = "price"
+            cell.stockAmount.text = "amount"
+            cell.stockValue.text = "value"
+        } else {
+            cell.stockTitle.text = "AAPL"
+            cell.stockPrice.text = "price"
+            cell.stockAmount.text = "amount"
+            cell.stockValue.text = "value"
+        }
+        return cell
     }
 }
