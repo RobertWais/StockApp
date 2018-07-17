839153367f1ec4a0d676a65c9eb920a193ea1815
RobertWais <wais.robert@uwlax.edu>
2018-07-17 15:02:40 -0700

    Created Company Class, CompanyCollectionViewCell

--- StockApp/Models/News.swift
+++ StockApp/Models/News.swift
@@ -11,15 +11,6 @@ import Foundation
 
 struct News {
     
-//    "title":"When Will This Stock Market Bubble Burst in Our Faces? Not Anytime Soon.",
-//    "pubDate":"2018-07-17 15:06:07",
-//    "link":"https://finance.yahoo.com/news/stock-market-bubble-burst-faces-150607862.html?.tsrc=rss",
-//    "guid":"82913fa4-7b53-30e3-8dba-1beaa29e8667",
-//    "author":"",
-//    "thumbnail":"",
-//    "description":"Never let it be said former Republican Congressman Ron Paul doesn\u2019t have a knack for theatrics.  Just for the record, however, this is a warning bell Ron Paul has been ringing since 2014 \u2014 and really, before that \u2014 and so far it has not happened.  With Q2 earnings projected to rise a hefty 26.7% year-over-year and with expectations of Q2 GDP growth rate in excess of 4%, it seems unlikely the market, and the economy itself, are more vulnerable now than they were then.",
-//    "content":"Never let it be said former Republican Congressman Ron Paul doesn\u2019t have a knack for theatrics.  Just for the record, however, this is a warning bell Ron Paul has been ringing since 2014 \u2014 and really, before that \u2014 and so far it has not happened.  With Q2 earnings projected to rise a hefty 26.7% year-over-year and with expectations of Q2 GDP gro
-//
     init(dict: [String: Any]) {
         self._title = dict["title"] as? String
         self._pubDate = dict["pubDate"] as? String
---
+++ StockApp/TableViewCells/CompanyCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CompanyCollectionViewCell.swift
+//  StockApp
+//
+//  Created by Robert Wais on 7/17/18.
+//  Copyright © 2018 MakeSchoolHackathon. All rights reserved.
+//
+
+import UIKit
+
+//protocol CompanyCollectionCellDelegate: class {
+//    func companySelected(on cell:CompanyCollectionViewCell)
+//}
+
+class CompanyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var companyTitle: UILabel!
+    
+    
+    func configureCell(){
+        
+    }
+    
+}
