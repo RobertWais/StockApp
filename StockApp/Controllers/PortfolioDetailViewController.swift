fd04cc51c767f62f73c39955987bcabfa5ba5e5d
Kevin <kevin94singh@gmail.com>
2018-07-18 10:45:26 -0700

    Portfolio class created.

---
+++ StockApp/Controllers/PortfolioDetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PortfolioDetailViewController.swift
+//  StockApp
+//
+//  Created by Kevin Singh on 7/18/18.
+//  Copyright © 2018 MakeSchoolHackathon. All rights reserved.
+//
+
+import UIKit
+
+class PortfolioDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if #available(iOS 11.0, *) {
+            navigationController?.navigationBar.prefersLargeTitles = true
+            navigationItem.largeTitleDisplayMode = .automatic
+            navigationController?.navigationBar.barStyle = .black
+        }
+        self.navigationController?.navigationBar.tintColor = UIColor.white
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
